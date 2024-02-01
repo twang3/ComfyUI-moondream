66f08647c4eb8968b5ebc6a36ba0a8fc576c9868
Kijai <40791699+kijai@users.noreply.github.com>
2024-02-01 13:20:08 +0200

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,10 +1,15 @@
 import os
 from PIL import Image
+import torch
 import numpy as np
 import comfy.utils
 import comfy.model_management
 
-from .moondream import VisionEncoder, TextModel
+from .moondream import Moondream
+from transformers import (
+    TextIteratorStreamer,
+    CodeGenTokenizerFast as Tokenizer,
+)
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -25,8 +30,9 @@ class MoondreamQuery:
 
     def process(self, images, question):
         batch_size = images.shape[0]
-        
         device = comfy.model_management.get_torch_device()
+        dtype = torch.float16 if comfy.model_management.should_use_fp16() and not comfy.model_management.is_device_mps(device) else torch.float32
+        
         checkpoint_path = os.path.join(script_directory, f"checkpoints/moondream1")
 
         if os.path.exists(checkpoint_path):
@@ -34,19 +40,20 @@ class MoondreamQuery:
         else:
             try:
                 from huggingface_hub import snapshot_download
-                snapshot_download(repo_id=f"vikhyatk/moondream1", local_dir=checkpoint_path, local_dir_use_symlinks=False)
+                snapshot_download(repo_id=f"vikhyatk/moondream1", ignore_patterns=["*.pt","*.bin"],local_dir=checkpoint_path, local_dir_use_symlinks=False)
             except:
                 raise FileNotFoundError("No model found.")
        
+        tokenizer = Tokenizer.from_pretrained(checkpoint_path)
+        moondream = Moondream.from_pretrained(checkpoint_path).to(device=device, dtype=dtype)
+        moondream.eval()
 
-        vision_encoder = VisionEncoder(checkpoint_path)
-        text_model = TextModel(checkpoint_path)
         answer_dict = {}
         if batch_size > 1:
             for i in range(batch_size):
                 image = Image.fromarray(np.clip(255. * images[i].cpu().numpy(),0,255).astype(np.uint8))
-                image_embeds = vision_encoder(image)
-                answer = text_model.answer_question(image_embeds, question)
+                image_embeds = moondream.encode_image(image)
+                answer = moondream.answer_question(image_embeds, question, tokenizer)
                 answer_dict[str(i)] = answer
 
             formatted_answers = ",\n".join([f'"{frame}" : "{answer}"' for frame, answer in answer_dict.items()])
@@ -55,8 +62,8 @@ class MoondreamQuery:
             return formatted_output,
         else:
             image = Image.fromarray(np.clip(255. * images[0].cpu().numpy(),0,255).astype(np.uint8))
-            image_embeds = vision_encoder(image)
-            answer = text_model.answer_question(image_embeds, question)
+            image_embeds = moondream.encode_image(image)
+            answer = moondream.answer_question(image_embeds, question, tokenizer)
             return answer,
         
 
